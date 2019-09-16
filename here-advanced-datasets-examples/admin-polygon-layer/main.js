af1067f33ada02c72abe27f35e8409da19c1dd7c
Postnov <pavel.postnov@here.com>
2019-09-16 16:55:18 +0300

    added an interactive map

--- here-advanced-datasets-examples/admin-polygon-layer/main.js
+++ here-advanced-datasets-examples/admin-polygon-layer/main.js
@@ -1,44 +1,22 @@
-                  
-/**
- * Moves the map to display over Berlin
- *
- * @param  {H.Map} map      A HERE Map instance within the application
- */
-function moveMapToBerlin(map){
-    map.setCenter({lat:52.5159, lng:13.3777});
-    map.setZoom(14);
-  }
-  
-  /**
-   * Boilerplate map initialization code starts below:
-   */
-  
-  //Step 1: initialize communication with the platform
-  // In your own code, replace variable window.apikey with your own apikey
-  var platform = new H.service.Platform({
-    apikey: 'YOUR_APIKEY'
+// Initialize the platform object:
+var platform = new H.service.Platform({
+  'apikey': 'mMAe2WEZear4kBv6DXbM1vHG03tQjKKsGcuS19rdGHQ'
   });
-  var defaultLayers = platform.createDefaultLayers();
-  
-  //Step 2: initialize a map - this map is centered over Europe
-  var map = new H.Map(document.getElementById('map'),
-    defaultLayers.vector.normal.map,{
-    center: {lat:50, lng:5},
-    zoom: 4,
-    pixelRatio: window.devicePixelRatio || 1
+
+// Obtain the default map types from the platform object
+var maptypes = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+  document.getElementById('mapContainer'),
+  maptypes.vector.normal.map,
+  {
+    zoom: 11.5,
+    center: { lng: 37.0958, lat: 55.8646 }
   });
-  // add a resize listener to make sure that the map occupies the whole container
-  window.addEventListener('resize', () => map.getViewPort().resize());
-  
-  //Step 3: make the map interactive
-  // MapEvents enables the event system
-  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-  
-  // Create the default UI components
-  var ui = H.ui.UI.createDefault(map, defaultLayers);
-  
-  // Now use the map as required...
-  window.onload = function () {
-    moveMapToBerlin(map);
-  }                  
+
+// Make the map interactive  
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+// Create the default UI:
+var ui = H.ui.UI.createDefault(map, maptypes);
